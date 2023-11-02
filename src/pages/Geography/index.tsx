1fe7b2f71a6de10b0bc518b0390a2ca3cc3a7a34
Welinton Dias <welinton.dias@rehagro.edu.br>
2023-11-02 16:45:46 -0300

    feat: Geography page border

--- src/pages/Geography/index.tsx
+++ src/pages/Geography/index.tsx
@@ -1,15 +1,19 @@
-import { Box } from '@mui/material';
+import { Box, useTheme } from '@mui/material';
 
 import { GeographyChart } from '@/components/Charts/GeographyChart';
 import { Header } from '@/components/Header';
 import { mockGeographyData } from '@/data/mockData';
 import { geoFeatures } from '@/data/mockGeoFeatures';
+import { tokens } from '@/theme';
 
 export const Geography = () => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
     return (
         <Box m="20px">
             <Header title="Geography Chart" subtitle="Simple Geography Chart" />
-            <Box height="75vh">
+            <Box height="75vh" border={`1px solid ${colors.grey[100]}`} borderRadius="4px">
                 <GeographyChart data={mockGeographyData} geoFeatures={geoFeatures} />
             </Box>
         </Box>
